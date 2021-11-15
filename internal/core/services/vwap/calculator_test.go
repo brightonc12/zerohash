b3d44382c20e50331adbd35195665c5d01ea3153
brighton <bchabula@gmail.com>
2021-11-15 11:14:12 +0200

    - updated calculator test

--- internal/core/services/vwap/calculator_test.go
+++ internal/core/services/vwap/calculator_test.go
@@ -1,6 +1,8 @@
 package vwap
 
 import (
+	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 	"zerohash/internal/core/services/numbers"
 	"zerohash/internal/domain"
@@ -15,38 +17,67 @@ var (
 	}
 )
 
+func newTestTrade(currency *domain.Currency) *domain.Trade {
+	return &domain.Trade{
+		Currency: currency.Name,
+		Price:    numbers.NewBigFloat(40.3),
+		Quantity: numbers.NewBigFloat(2),
+	}
+}
+
 func TestImpl_PublishToSingleCalcChan(t *testing.T) {
 	tests := []struct {
 		name     string
 		expected bool
 	}{
-		{name: "before time", expected: false},
+		{name: "publish to chan success", expected: false},
 	}
 
-	tradeChan := make(chan *domain.Trade)
 	currencies := []*domain.Currency{
-		domain.Currencies.EthBtc,
 		domain.Currencies.BtcUsd,
+		domain.Currencies.EthBtc,
 	}
 
-	_ = New(tradeChan, currencies)
+	testTrades := []*domain.Trade{
+		newTestTrade(domain.Currencies.BtcUsd),
+		newTestTrade(domain.Currencies.EthBtc),
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tradeChan := make(chan *domain.Trade)
+			srv := New(tradeChan, currencies)
 
 			calcChan := make(map[string]Calculator)
 			for _, c := range currencies {
 				newTradeChan := make(chan *domain.Trade)
 				calcChan[c.Name] = NewCalculator(newTradeChan, testWindow)
 			}
-			//srv.PublishToSingleCalcChan()
-		})
+			go func() {
+				for _, trd := range testTrades {
+					tradeChan <- trd
+				}
+				close(tradeChan)
+			}()
 
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
+			go srv.PublishToSingleCalcChan(calcChan, wg)
+
+			td := <-calcChan[domain.Currencies.BtcUsd.Name].GetTradeChan()
+			assert.Equal(t, td.Currency, domain.Currencies.BtcUsd.Name)
+
+			td = <-calcChan[domain.Currencies.EthBtc.Name].GetTradeChan()
+			assert.Equal(t, td.Currency, domain.Currencies.EthBtc.Name)
+		})
 	}
 }
 
-func TestCalculator_UpdateAgainstTrade(t *testing.T) {
-}
-
 func TestCalculator_GetTradeChan(t *testing.T) {
+	tradeChan := make(chan *domain.Trade)
+	calc := NewCalculator(tradeChan, 10)
+
+	t.Run("get same private channel ", func(t *testing.T) {
+		assert.Equal(t, calc.GetTradeChan(), tradeChan)
+	})
 }
